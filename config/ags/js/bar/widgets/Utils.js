14b0524927e016cc359713c1ecfca4eaec5e5663
Luks17 <lucasthidavort@gmail.com>
2024-01-02 11:45:09 -0400

    Cycle wallpaper btn

--- config/ags/js/bar/widgets/Utils.js
+++ config/ags/js/bar/widgets/Utils.js
@@ -2,7 +2,14 @@ import icons from "../../icons.js";
 import FontIcon from "../../misc/FontIcon.js";
 import HoverableBtn from "../../misc/HoverableBtn.js";
 import App from "resource:///com/github/Aylur/ags/app.js";
+import wallpaper from "../../services/wallpaper.js";
 import { Widget } from "resource:///com/github/Aylur/ags/widget.js";
+
+const WallpaperBtn = () => HoverableBtn({
+  class_name: "wallpaper-button",
+  label: "  ",
+  on_clicked: () => wallpaper.cycle(),
+})
 
 const ClipboardBtn = () => HoverableBtn({
   class_name: "clipboard-button",
@@ -17,5 +24,6 @@ export default () => Widget.Box({
   class_name: 'utils-panel',
   children: [
     ClipboardBtn(),
+    WallpaperBtn()
   ],
 });
