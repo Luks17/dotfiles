2b3f3208a369e1b7a018f12581f8a548dc5738b8
Luks17 <lucasthidavort@gmail.com>
2024-04-03 14:28:37 -0400

    Added different tabwidth depending on filetype of open buffer

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -3,6 +3,30 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
+    autocmds = {
+      autodefinetabwidth = {
+        {
+          event = "User",
+          pattern = "AstroBufsUpdated",
+          desc = "Define tab width as 4 when a php or rust file is opened, otherwise set it as 2",
+          group = "autodefinetabwidth",
+
+          callback = function()
+            local filetype = vim.bo.filetype
+            local tabwidth = 2
+
+            if filetype == "php" or filetype == "rust" then
+              tabwidth = 4
+            end
+
+            vim.bo.tabstop = tabwidth
+            vim.bo.shiftwidth = tabwidth
+            vim.bo.softtabstop = tabwidth
+            vim.bo.expandtab = true
+          end
+        }
+      }
+    },
     diagnostics = {
       virtual_text = false
     },
@@ -15,7 +39,7 @@ return {
         L = { function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
         H = { function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer" },
 
-        -- Toggle Virtual Blame mapping overriding Single Line Blame
+        -- Toggle Virtual Blame mapping, overriding Single Line Blame
         ["<Leader>gl"] = { "<cmd>ToggleBlame virtual<cr>", desc = "Toggle Virtual Blame" },
       },
     }
