4cdf6642b1b819f9fe784f66e57ec59e81191546
Luks17 <lucasthidavort@gmail.com>
2024-08-17 19:55:36 -0400

    feat: kbdlayout service from old config

--- config/ags/main.ts
+++ config/ags/main.ts
@@ -14,6 +14,7 @@ import Verification from "widget/powermenu/Verification"
 import { forMonitors } from "lib/utils"
 import { setupQuickSettings } from "widget/quicksettings/QuickSettings"
 import { setupDateMenu } from "widget/datemenu/DateMenu"
+import "service/keyboardlayout"
 
 App.config({
     onConfigParsed: () => {
