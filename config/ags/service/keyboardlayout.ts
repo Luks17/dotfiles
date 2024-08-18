c4c9c05774691aeb3e10108f513cb16ce1a1ed40
Luks17 <lucasthidavort@gmail.com>
2024-08-17 21:55:14 -0400

    feat: removed jsdocs

--- config/ags/service/keyboardlayout.ts
+++ config/ags/service/keyboardlayout.ts
@@ -4,41 +4,40 @@ import { execAsync } from "resource:///com/github/Aylur/ags/utils.js";
 const layouts = ["English", "Português"];
 
 class KeyboardLayout extends Service {
-	static {
-		Service.register(this, {}, {});
-	}
+  static {
+    Service.register(this, {}, {});
+  }
 
-	#current = 0;
+  #current = 0;
 
-	/** @type {string} */
-	#layout = "";
+  #layout = "";
 
-	get layout() {
-		return this.#layout;
-	}
+  get layout() {
+    return this.#layout;
+  }
 
-	#notify() {
-		execAsync(`notify-send "Keyboard Layout" "${this.#layout}"`).catch((e) =>
-			print(e),
-		);
-	}
+  #notify() {
+    execAsync(`notify-send "Keyboard Layout" "${this.#layout}"`).catch((e) =>
+      print(e),
+    );
+  }
 
-	async next() {
-		this.#current = (this.#current + 1) % layouts.length;
+  async next() {
+    this.#current = (this.#current + 1) % layouts.length;
 
-		this.#layout = layouts[this.#current];
+    this.#layout = layouts[this.#current];
 
-		await execAsync(
-			"hyprctl switchxkblayout at-translated-set-2-keyboard next",
-		).catch((e) => print(e));
-		this.#notify();
-	}
+    await execAsync(
+      "hyprctl switchxkblayout at-translated-set-2-keyboard next",
+    ).catch((e) => print(e));
+    this.#notify();
+  }
 
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.#layout = layouts[this.#current];
-	}
+    this.#layout = layouts[this.#current];
+  }
 }
 
 const keyboardlayout = new KeyboardLayout();
