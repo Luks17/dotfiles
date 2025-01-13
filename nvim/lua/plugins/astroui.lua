ebd1452e30e0abe33ab59f327aa98444437cbb37
Luks17 <lucasthidavort@gmail.com>
2025-01-13 15:00:37 -0400

    chore(nvim): updates

--- nvim/lua/plugins/astroui.lua
+++ nvim/lua/plugins/astroui.lua
@@ -3,7 +3,7 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = "tokyonight-moon",
+    colorscheme = "tokyonight-night",
     icons = {
       LSPLoading1 = "⠋",
       LSPLoading2 = "⠙",
