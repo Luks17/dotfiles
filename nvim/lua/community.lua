8c3de8c0c7adbfeb246e4f2d945685297876f980
Luks17 <lucasthidavort@gmail.com>
2025-01-25 22:41:38 -0400

    nvim: disable tabline

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -1,12 +1,15 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.diagnostics.tiny-inline-diagnostic-nvim" },
-  { import = "astrocommunity.motion.hop-nvim" },
-  { import = "astrocommunity.media.image-nvim" },
-  { import = "astrocommunity.search.nvim-spectre" },
+
   { import = "astrocommunity.completion.supermaven-nvim" },
-  { import = "astrocommunity.test.vim-test" },
+
+  { import = "astrocommunity.diagnostics.tiny-inline-diagnostic-nvim" },
+
+  { import = "astrocommunity.media.image-nvim" },
+
+  { import = "astrocommunity.motion.hop-nvim" },
+
   { import = "astrocommunity.pack.laravel" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.rust" },
@@ -14,4 +17,10 @@ return {
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.vue" },
+
+  { import = "astrocommunity.recipes.disable-tabline" },
+
+  { import = "astrocommunity.search.nvim-spectre" },
+
+  { import = "astrocommunity.test.vim-test" },
 }
