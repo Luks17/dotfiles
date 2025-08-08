9bcdb2bb692bfc3adda24affd1a406372439c82b
Luks17 <lucasthidavort@gmail.com>
2025-08-07 21:43:10 -0400

    feat(lsp): setup elixirls manually

--- config/nvim/lua/plugins/astrolsp.lua
+++ config/nvim/lua/plugins/astrolsp.lua
@@ -4,12 +4,21 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
-  ---@type AstroLSPOpts
-  opts = {
-    formatting = {
+  ---@param opts AstroLSPOpts
+  opts = function(_, opts)
+    opts.servers = opts.servers or {}
+    table.insert(opts.servers, "elixirls")
+
+    opts.config = require("astrocore").extend_tbl(opts.config or {}, {
+      elixirls = {
+        cmd = { "/usr/lib/elixir-ls/language_server.sh" },
+      },
+    })
+
+    opts.formatting = {
       disabled = {
         "volar",
       },
-    },
-  },
+    }
+  end,
 }
