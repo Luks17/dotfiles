3fa7779677dda6b93e83b215c216e40eaad94a65
Luks17 <lucasthidavort@gmail.com>
2026-07-17 11:13:28 -0400

    kanagawa

--- config/nvim/plugin/09_colorscheme.lua
+++ config/nvim/plugin/09_colorscheme.lua
@@ -1,17 +1,17 @@
 Setup.now(function()
     vim.pack.add({
-        'https://github.com/rose-pine/neovim',
-        -- 'https://github.com/rebelot/kanagawa.nvim'
+        -- 'https://github.com/rose-pine/neovim',
+        'https://github.com/rebelot/kanagawa.nvim',
     })
 
-    require('rose-pine').setup()
+    -- require('rose-pine').setup()
 
-    -- require('kanagawa').setup({
-    --     -- disables line numbers bg
-    --     colors = {
-    --         theme = { all = { ui = { bg_gutter = 'none' } } },
-    --     },
-    -- })
+    require('kanagawa').setup({
+        -- disables line numbers bg
+        colors = {
+            theme = { all = { ui = { bg_gutter = 'none' } } },
+        },
+    })
 
-    vim.cmd('colorscheme rose-pine')
+    vim.cmd('colorscheme kanagawa')
 end)
